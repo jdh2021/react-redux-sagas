d4c40e6eaaf9161921c5cd09da1feb0c40fae648
Jennifer Hightower <jenniferhightower2020@gmail.com>
2022-09-28 13:41:26 -0500

    created initial SingleMovieDetail component. added routing in App.js. created button onClick to return to MovieList.

---
+++ src/components/SingleMovieDetail/SingleMovieDetail.jsx
@@ -0,0 +1,26 @@
+import { useHistory } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+
+const SingleMovieDetail = () => {
+    // use history to navigate between pages
+    const history = useHistory();
+    // use dispatch to report action to store
+    const dispatch = useDispatch();
+
+    // useEffect to dispatch action to store when SingleMovieDetail component renders
+    useEffect(() => {
+        dispatch({ });
+    }, []);
+
+    const goToMovieList = () => {
+        history.push('/');
+    }
+
+    return (
+        <button onClick={goToMovieList}>Back to List</button>
+    )
+
+}
+
+export default SingleMovieDetail;
